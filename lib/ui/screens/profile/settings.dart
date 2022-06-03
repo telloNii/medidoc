84725f5cd4837708188e2904fea97821c90ae37b
Todd Nelson <tello_nii@outlook.com>
2022-06-03 16:04:25 +0000

    created a settings page

--- lib/ui/screens/profile/settings.dart
+++ lib/ui/screens/profile/settings.dart
@@ -1,24 +1,181 @@
+import 'package:doctor_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class Settings extends StatefulWidget {
-  static const String id = " settings page route";
-  const Settings({Key? key}) : super(key: key);
+class ProfileSettings extends StatefulWidget {
+  static const String id = "lib/src/pages/ProfileSettings/ProfileSettings1.dart";
+
+  const ProfileSettings({Key? key}) : super(key: key);
 
   @override
-  State<Settings> createState() => _SettingsState();
+  _ProfileSettingsState createState() => _ProfileSettingsState();
 }
 
-class _SettingsState extends State<Settings> {
+class _ProfileSettingsState extends State<ProfileSettings> {
+  late bool _dark;
+
+  @override
+  void initState() {
+    super.initState();
+    _dark = false;
+  }
+
+  Brightness _getBrightness() {
+    return _dark ? Brightness.dark : Brightness.light;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-        child: Column(
-          children: [],
+    return Theme(
+      data: ThemeData(
+        brightness: _getBrightness(),
+      ),
+      child: Scaffold(
+        backgroundColor: _dark ? null : Colors.grey.shade200,
+        appBar: AppBar(
+          elevation: 0,
+          brightness: _getBrightness(),
+          iconTheme: IconThemeData(color: _dark ? Colors.white : Colors.black),
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Settings',
+            style: TextStyle(color: _dark ? Colors.white : Colors.black),
+          ),
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Card(
+                    elevation: 8.0,
+                    shape:
+                        RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                    color: primaryColor,
+                    child: ListTile(
+                      onTap: () {
+                        //open edit profile
+                      },
+                      title: const Text(
+                        "TestUser 3",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      leading: const CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/user_pic.png'),
+                      ),
+                      trailing: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Card(
+                    elevation: 4.0,
+                    margin: const EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 16.0),
+                    shape:
+                        RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: const Icon(
+                            Icons.lock_outline,
+                            color: primaryColor,
+                          ),
+                          title: const Text("Change Password"),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change password
+                          },
+                        ),
+                        _buildDivider(),
+                        ListTile(
+                          leading: const Icon(
+                            FontAwesomeIcons.language,
+                            color: primaryColor,
+                          ),
+                          title: const Text("Change Language"),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change language
+                          },
+                        ),
+                        _buildDivider(),
+                        ListTile(
+                          leading: const Icon(
+                            Icons.location_on,
+                            color: primaryColor,
+                          ),
+                          title: const Text("Change Location"),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change location
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Text(
+                    "Notification ProfileSettings",
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  SwitchListTile(
+                    activeColor: primaryColor,
+                    contentPadding: const EdgeInsets.all(0),
+                    value: true,
+                    title: const Text("Received notification"),
+                    onChanged: (val) {},
+                  ),
+                  const SwitchListTile(
+                    activeColor: primaryColor,
+                    contentPadding: EdgeInsets.all(0),
+                    value: false,
+                    title: const Text("Received newsletter"),
+                    onChanged: null,
+                  ),
+                  SwitchListTile(
+                    activeColor: primaryColor,
+                    contentPadding: const EdgeInsets.all(0),
+                    value: true,
+                    title: const Text("Received Offer Notification"),
+                    onChanged: (val) {},
+                  ),
+                  const SwitchListTile(
+                    activeColor: primaryColor,
+                    contentPadding: EdgeInsets.all(0),
+                    value: true,
+                    title: const Text("Received App Updates"),
+                    onChanged: null,
+                  ),
+                  const SizedBox(height: 60.0),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Container _buildDivider() {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 8.0,
+      ),
+      width: double.infinity,
+      height: 1.0,
+      color: Colors.grey.shade400,
     );
   }
 }
